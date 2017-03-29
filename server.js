349613e55d0d2ceeb63e14d7f34872a271ee42e4
Kristopher Parrillo <kkparrillo@gmail.com>
2017-03-28 17:39:53 -0700

    commented out the headers

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use( (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-});
+// app.use( (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+// });
 
 // ROUTES
 app.use('/', routes);
